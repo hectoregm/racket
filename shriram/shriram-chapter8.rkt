2d65d22b93451dc4d98cd36a0e5ba97e5e4d91db
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-03-15 18:03:19 -0600

    Added strict function and some new tests

--- shriram/shriram-chapter8.rkt
+++ shriram/shriram-chapter8.rkt
@@ -59,16 +59,16 @@
     [(list? sexp)
      (case (first sexp)
        [(+) (addS (parse (second sexp))
-                 (parse (third sexp)))]
+                  (parse (third sexp)))]
        [(-) (subS (parse (second sexp))
-                 (parse (third sexp)))]
+                  (parse (third sexp)))]
        [(with) (withS (first (second sexp))
-                     (parse (second (second sexp)))
-                     (parse (third sexp)))]
+                      (parse (second (second sexp)))
+                      (parse (third sexp)))]
        [(fun) (funS (first (second sexp))
-                   (parse (third sexp)))]
+                    (parse (third sexp)))]
        [else (appS (parse (first sexp))
-                  (parse (second sexp)))])]))
+                   (parse (second sexp)))])]))
 
 (define (desugar expr)
   (type-case CFAES/L expr
@@ -93,28 +93,35 @@
 
 ;; num+ CFAE/L-Value CFAE/L-Value -> CFAE/L-Value
 (define (num+ numa numb)
-  (numV (+ (numV-n numa)
-           (numV-n numb))))
+  (numV (+ (numV-n (strict numa))
+           (numV-n (strict numb)))))
 
 (define (num- numa numb)
-  (numV (- (numV-n numa)
-           (numV-n numb))))
+  (numV (- (numV-n (strict numa))
+           (numV-n (strict numb)))))
+
+;; strict : CFAE/L-Value -> CFAE/L-Value [excluding exprV]
+(define (strict e)
+  (type-case CFAE/L-Value e
+    [exprV (expr env) (strict (interp expr env))]
+    [else e]))
 
 ;; interp : CFAE/L Env -> CFAE/L-Value
-(define (interp expr ds)
+(define (interp expr env)
   (type-case CFAE/L expr
     [num (n) (numV n)]
-    [add (l r) (num+ (interp l ds) (interp r ds))]
-    [sub (l r) (num- (interp l ds) (interp r ds))]
-    [id (v) (lookup v ds)]
+    [add (l r) (num+ (interp l env) (interp r env))]
+    [sub (l r) (num- (interp l env) (interp r env))]
+    [id (v) (lookup v env)]
     [fun (bound-id bound-body)
-         (closureV bound-id bound-body ds)]
+         (closureV bound-id bound-body env)]
     [app (fun-expr arg-expr)
-         (local ([define fun-val (interp fun-expr ds)])
+         (local ([define fun-val (strict (interp fun-expr env))]
+                 [define arg-val (exprV arg-expr env)])
            (if (closureV? fun-val)
                (interp (closureV-body fun-val)
                        (aSub (closureV-param fun-val)
-                             (interp arg-expr ds)
+                             arg-val
                              (closureV-env fun-val)))
                (error 'interp (string-append (~a fun-expr) " expression is not a function"))))]))
 (test (interp (desugar (parse '3)) (mtSub)) (numV 3))
@@ -128,10 +135,11 @@
 (test (interp (desugar (parse '{with {x 5} {+ x {with {x 3} 10}}})) (mtSub)) (numV 15))
 (test (interp (desugar (parse '{with {x 5} {+ x {with {x 3} x}}})) (mtSub)) (numV 8))
 (test (interp (desugar (parse '{with {x 5} {+ x {with {y 3} x}}})) (mtSub)) (numV 10))
-(test (interp (desugar (parse '{with {x 5} {with {y x} y}})) (mtSub)) (numV 5))
-(test (interp (desugar (parse '{with {x 5} {with {x x} x}})) (mtSub)) (numV 5))
-(test (interp (desugar (parse '{{fun {x} x} 3})) (mtSub)) (numV 3))
+(test (strict (interp (desugar (parse '{with {x 5} {with {y x} y}})) (mtSub))) (numV 5))
+(test (strict (interp (desugar (parse '{with {x 5} {with {x x} x}})) (mtSub))) (numV 5))
+(test (strict (interp (desugar (parse '{{fun {x} x} 3})) (mtSub))) (numV 3))
 (test (interp (desugar (parse '{{{fun {x} x} {fun {x} {+ x 5}}} 3})) (mtSub)) (numV 8))
-(test (interp (desugar (parse '{with {x 3} {fun {y} {+ x y}}})) (mtSub)) (closureV 'y (add (id 'x) (id 'y)) (aSub 'x (numV 3) (mtSub))))
+(test (interp (desugar (parse '{with {x 3} {fun {y} {+ x y}}})) (mtSub)) (closureV 'y (add (id 'x) (id 'y)) (aSub 'x (exprV (num 3) (mtSub)) (mtSub))))
 (test (interp (desugar (parse '{with {x 3} {with {f {fun {y} {+ x y}}} {with {x 5} {f 4}}}})) (mtSub)) (numV 7))
 (test (interp (desugar (parse '{with {x 10} {{fun {y} {+ y x}} {+ 5 x}}})) (mtSub)) (numV 25))
+(test (interp (cparser '{with {double {fun {x} {+ x x}}} {+ {double 5} {double 10}}}) (mtSub)) (numV 30))
