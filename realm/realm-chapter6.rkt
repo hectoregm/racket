8a23d24a42c29790d89cada7781ac951b8b7b979
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-24 14:26:36 -0500

    Remove backup file

--- realm/realm-chapter6.rkt
+++ realm/realm-chapter6.rkt
@@ -1,20 +1,45 @@
 #lang racket
 
-(define (start n m)
-  (set! lower (min n m))
-  (set! upper (max n m))
-  (guess))
+(require 2htdp/image 2htdp/universe)
 
-(define lower 1)
-(define upper 100)
+(struct pit (snake goos))
 
-(define (guess)
-  (quotient (+ lower upper) 2))
+(struct snake (dir segs))
 
-(define (smaller)
-  (set! upper (max lower (sub1 (guess))))
-  (guess))
+(struct posn (x y))
 
-(define (bigger)
-  (set! lower (min upper (add1 (guess))))
-  (guess))
+(struct goo (loc expire))
+
+(define (start-snake)
+  (big-bang (pit (snake "right" (list (posn 1 1)))
+                 (list (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)))
+            (on-tick next-pit TICK-RATE)
+            (on-key direct-snake)
+            (to-draw render-pit)
+            (stop-when dead? render-end)))
+
+(define (next-pit w)
+  (define snake (pit-snake w))
+  (define goos (pit-goos w))
+  (define goo-to-eat (can-eat snake goos))
+  (if goo-to-eat
+      (pit (grow snake) (age-goo (eat goos goo-to-eat)))
+      (pit (slither snake) (age-goo goos))))
+
+(define (can-eat snake goos)
+  (cond
+    [(empty? goos) #f]
+    [else (if (close? (snake-head snake) (first goos))
+              (first goos)
+              (can-eat snake (rest goos)))]))
+
+(define (close? s g)
+  (posn=? s (goo-loc g)))
+
+(define (eat goos goo-to-eat)
+  (cons (fresh-goo) (remove goo-to-eat goos)))
